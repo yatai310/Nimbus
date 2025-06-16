41bec19db2ab9e39d89c47713837e01ce501bd99
asaiharuhiko <0120mmasai@gmail.com>
2025-06-16 15:48:45 +0900

    ロード画面を追加【大歓喜】
    ロード画面を追加することによって、ゲームへの没入感を大規模に追加することに成功しました。
    しかしながら、ローディングのプログレスバーに関しては一瞬でmaxになるため、必要性がわからないのである。

--- Assets/Scripts/Title/Buttons/PlayBotton.cs
+++ Assets/Scripts/Title/Buttons/PlayBotton.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class PlayBotton : MonoBehaviour
 {
     public static string StageName = "GameScene";
+    public Slider progressBar;
+    public GameObject NowLoding;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,13 +20,27 @@ public class PlayBotton : MonoBehaviour
     {
 
     }
-    public void OnClick()
-    {
-        Debug.Log("クリックされたメーン");
-    }
     public void Scenemg()
     {
         Debug.Log("Scenemg");
-        SceneManager.LoadScene(StageName);
+        StartCoroutine(LoadSceneAsync());
+    }
+    IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(StageName);
+        operation.allowSceneActivation = false;
+        NowLoding.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress;
+            progressBar.value = progress / 0.9f;
+            Debug.Log("Loading progress: " + progress);
+            if (progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(1f);
+                operation.allowSceneActivation = true;
+                NowLoding.SetActive(false);
+            }
+        }
     }
 }
